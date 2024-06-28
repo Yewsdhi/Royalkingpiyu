766740c6ab88d7f21fcfb91386b8405ca9777766
The-stonex <147587252+the-stonex@users.noreply.github.com>
2024-06-28 21:32:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,7 +67,7 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/royalmusibotl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+PjJLYgbawLc5ZWZl")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/royalmusicboto")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
