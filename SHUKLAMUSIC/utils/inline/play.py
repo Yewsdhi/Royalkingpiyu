2be79ef4d302bc70c1579aef83c80baef7b11f75
The-stonex <147587252+the-stonex@users.noreply.github.com>
2024-06-28 21:47:03 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -68,7 +68,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
          InlineKeyboardButton(text="😈 𝐎𝐰𝐧𝐞𝐫 😈", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/+-kj9BEEhruUxMDNl"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -85,7 +85,7 @@ def stream_markup(_, chat_id):
         ],
         [
          InlineKeyboardButton(text="😈 𝐎𝐰𝐧𝐞𝐫 😈", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/+-kj9BEEhruUxMDNl"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
