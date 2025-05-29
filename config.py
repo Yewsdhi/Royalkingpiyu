4a8759891a017c06e0e496d95eb606cb44305e93
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-05-29 11:33:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/itz_Quiz")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ABOUT_CHAHAT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_DPZ_WORLDS_ll")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/queenbotgrup")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
